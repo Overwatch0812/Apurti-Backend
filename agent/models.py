798274c5f97a923fd32eb7402fa0ace4f016b9c0
Sahil Nikshe <nikshesahil@gmail.com>
2025-02-15 18:26:32 +0530

    Auth

--- agent/models.py
+++ agent/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+# humidityRange: "25-35"
+# numberOfSections: "4"
+# operatingHours: "8:00-12:00"
+# securityLevel: "low"
+# specialStorage: "Cold Storage"
+# staffCount: "3"
+# storageCapacity: "850"
+# temperatureRange: "30-40"
+# totalArea: "1000"
+# warehouseName: "Nikshe"
+
+
 # Create your models here.
+class Warehouse(models.Model):
+    pass
